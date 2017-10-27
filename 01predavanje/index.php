a20dcbd40ded6a8ef8c6bfa8f125ecd88023354e
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2017-10-27 23:19:18 +0200

    Added lecture 01

---
+++ 01predavanje/index.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+1. Kako u nacelu funkcionira Internet
+2. Kako radi DNS
+3. Sto je server, server-side i client-side izvrsavanje koda
+4. PHP kao jedan od mnogih server-side jezika
+5. Sto je HTML i CSS
+6. Osnovna HTML sintaksa, osnovni HTML elementi (h1, h2, h3, h4, h5 , h6, img, p, a, br, div)
+7. Implementacija gotovih librarya i frameworka - bootstrap
+8. Komentari u HTMLu i komentari u PHPu
+9. Ispis HTML-a kroz PHP
+10. XAMPP, Apache, MySql
+*/
+?>
+
+<!-- ovo je HTML komentar -->
+
+<html>
+  <head>
+    <title>Naslov stranice</title>
+  </head>
+  <body>
+    <p>Ovo je neki paragraph</p>
+    <h1>Ovo je h1 koji je jako vazan </h1>
+    <h6>Ovo je h6 koji je puno manje vazan</h6>
+    
+    <p>Slika koja ima izvor na drugoj stranici</p>
+    <img src="https://ssl.c.photoshelter.com/img-get/I0000DpeXraL7bvA/t/200/I0000DpeXraL7bvA.jpg">
+
+    <p>Slika s lokalnog servera</p>
+    <img src="bunny2.jpg">
+
+    <a href="http://google.com">Google link</a>
+  </body>
+</html>
