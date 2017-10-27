53441d11ec3e438c19beae8dd6a5f544efc83048
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2017-10-27 23:28:33 +0200

    updated lecture 01

---
+++ 01predavanje/README.md
@@ -0,0 +1,10 @@
+1. Kako u nacelu funkcionira Internet
+2. Kako radi DNS
+3. Sto je server, server-side i client-side izvrsavanje koda
+4. PHP kao jedan od mnogih server-side jezika
+5. Sto je HTML i CSS
+6. Osnovna HTML sintaksa, osnovni HTML elementi (h1, h2, h3, h4, h5 , h6, img, p, a, br, div)
+7. Implementacija gotovih librarya i frameworka - bootstrap
+8. Komentari u HTMLu i komentari u PHPu
+9. Ispis HTML-a kroz PHP
+10. XAMPP, Apache, MySql
--- 01predavanje/index.php
+++ 01predavanje/index.php
@@ -1,19 +1,3 @@
-<?php
-
-/*
-1. Kako u nacelu funkcionira Internet
-2. Kako radi DNS
-3. Sto je server, server-side i client-side izvrsavanje koda
-4. PHP kao jedan od mnogih server-side jezika
-5. Sto je HTML i CSS
-6. Osnovna HTML sintaksa, osnovni HTML elementi (h1, h2, h3, h4, h5 , h6, img, p, a, br, div)
-7. Implementacija gotovih librarya i frameworka - bootstrap
-8. Komentari u HTMLu i komentari u PHPu
-9. Ispis HTML-a kroz PHP
-10. XAMPP, Apache, MySql
-*/
-?>
-
 <!-- ovo je HTML komentar -->
 
 <html>
@@ -34,3 +18,7 @@
     <a href="http://google.com">Google link</a>
   </body>
 </html>
+
+<?php
+echo 'Ovo je ispis iz PHP jezika';
+?>
